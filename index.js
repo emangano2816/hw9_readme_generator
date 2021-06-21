f19b51e612cf0dafc738f53b9a143f1b5e116f1f
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-06-21 16:39:31 -0400

    Added code to init() function. Placed inquirer. code inside init(). Series of questions are displayed to the user. Response provides requested information.

--- index.js
+++ index.js
@@ -1,13 +1,81 @@
 // TODO: Include packages needed for this application
-const inquire = require("inquirer");
+const fs = require("fs");   //module included with node
+const inquirer = require("inquirer");
+const inquire = require("inquirer");  //npm package that is installed first
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt ([
+            {             
+                type: 'input',
+                message: 'What is the title of your project?',
+                name: 'title',
+                default: 'Project Title',
+            },
+            {
+                type: 'editor',
+                message: 'Provide a descriptoin for your project.',
+                name: 'description',
+                default: 'Description of project goes here.',
+            },
+            {
+                type: 'editor',
+                message: 'Provide installation instructions.',
+                name: 'installation',
+                default: 'Installation instructions go here.',
+            },
+            {   
+                type: 'editor',
+                message: 'Provide guidelines for contributing to the project.',
+                name: 'contributions',
+                default: 'Guidelines for contributing go here.',
+            },
+            {
+                type: 'editor',
+                message: 'Provide instructions for testing the application.',
+                name: 'tests',
+                default: 'Test instructions go here.',
+            },
+            {
+                type: 'list',
+                message: 'Choose a license for your application.',
+                name: 'license',
+                choices: [
+                            'Apache license 2.0', 
+                            'GNU Affero General Public License v3.0',
+                            'BSD 2-clause "Simplified" license',
+                            'BSD 3-clause Clear license',
+                            'GNU General Public License v2.0',
+                            'GNU General Public License v3.0',
+                            'GNU Lesser General Public License v3.0',
+                            'MIT',
+                            'The Unlicense'                     
+                        ],
+            },
+            {
+                type: 'input',
+                message: 'What is your GitHub username (link to repo will be included for questions)?',
+                name: 'questions',
+            },
+            {
+                type: 'input',
+                message: 'What is your email address?',
+                name: 'emailaddress',
+            }
+        ])
+        .then((response) => {
+            console.log(response);
+        })
+}
 
 // Function call to initialize app
 init();
