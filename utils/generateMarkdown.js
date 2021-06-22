44dd66a626c6c57278a8041f9d6667ce0babcef1
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-06-21 20:15:29 -0400

    Updated the renderLicenseBadge function.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,42 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+let badge;
+function renderLicenseBadge(license) {
+    switch (license) {
+        case 'Apache 2.0 License':
+            badge='https://img.shields.io/badge/License-Apache%202.0-blue.svg';
+            break;
+        case 'BSD 2-clause "Simplified" License':
+            badge='https://img.shields.io/badge/License-BSD%202--Clause-orange.svg';
+            break;
+        case 'BSD 3-clause Clear License':
+            badge='https://img.shields.io/badge/License-BSD%203--Clause-blue.svg';
+            break;
+        case 'GNU Affero General Public License v3.0':
+            badge='https://img.shields.io/badge/License-AGPL%20v3-blue.svg';
+            break;
+        case 'GNU General Public License v2.0':
+            badge='https://img.shields.io/badge/License-GPL%20v2-blue.svg';
+            break;
+        case 'GNU General Public License v3.0':
+            badge='https://img.shields.io/badge/License-GPLv3-blue.svg'
+            break;
+        case 'GNU Lesser General Public License v3.0':
+            badge='https://img.shields.io/badge/License-LGPL%20v3-blue.svg';
+            break;
+        case 'MIT':
+            badge='https://img.shields.io/badge/License-MIT-yellow.svg'
+            break;
+        case 'The Unlicense':
+            badge='https://img.shields.io/badge/license-Unlicense-blue.svg';
+            break;
+        default:
+            badge='';
+            break;
+    }
+    console.log(badge);
+    return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -18,5 +54,6 @@ function generateMarkdown(data) {
 }
 
 module.exports = {
-    // generateMarkdown();
+    badge,
+    renderLicenseBadge
 }
